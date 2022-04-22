de386931949d56af29bc008287177f0a4fa418e2
LayMui <mui_ume@hotmail.com>
2022-04-22 16:04:48 +0800

    Use stockprice.apk

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,2 +1,2 @@
-cp android/app/build/outputs/apk/debug/app-debug.apk tests/apps 
-cp android/app/build/outputs/apk/release/app-release.apk tests/apps 
+cp android/app/build/outputs/apk/debug/app-debug.apk tests/apps/stockprice_debug.apk
+cp android/app/build/outputs/apk/release/app-release.apk tests/apps/stockprice.apk
