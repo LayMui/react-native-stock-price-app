d5d7b7ab87e205bd5e1cd6c435e11343e10b2c1e
LayMui <mui_ume@hotmail.com>
2022-05-13 10:36:50 +0800

    Working with webdriverio cucumber only

--- tests/src/step-definitions/searchStockPrice.steps.ts
+++ tests/src/step-definitions/searchStockPrice.steps.ts
@@ -4,6 +4,7 @@ import { Given, Then, When } from '@cucumber/cucumber'
 import { Actor } from '@serenity-js/core'
 import { Search } from '../tasks/Search'
 
+import SearchPageWDIO  from '../page-objects/search.page'
 
 Given('{actor} is at the stock price app', (actor: Actor) => {})
 
@@ -11,7 +12,8 @@ When(
   '{pronoun} want to search for company {string}',
   { timeout: 80000 },
   async (actor: Actor, company: string) => {
-    return await actor.attemptsTo(Search.forCompany(company))
+   // return await actor.attemptsTo(Search.forCompany(company))
+   await SearchPageWDIO.searchForCompany(company)
   }
 )
 
